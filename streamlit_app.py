f9a4f3c617a169adb084e8146ebfa8cf48a5f296
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-02 20:41:28 +0800

    add button to test

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,11 @@ conn = st.connection("google_service_account", type = GSheetsConnection)
 
 df = conn.read(ttl="5m")
 
+def test():
+    st.write('success')
+
+
+
 st.title('AI Task Tracker')
 st.dataframe(df)
 st.data_editor(df)
@@ -14,3 +19,4 @@ taskName = st.text_input('Task Name')
 
 st.write('role')
 Role = st.text_input('Role')
+st.button('add', on_click = test)
