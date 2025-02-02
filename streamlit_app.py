e74276740943517ea3b997e980f56fa9e8929617
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-03 01:05:18 +0800

    put clear cache at the top

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
-conn = st.connection("google_service_account", type = GSheetsConnection)
+
 st.cache_resource.clear()
+conn = st.connection("google_service_account", type = GSheetsConnection)
+
 df = conn.read(ttl="5m")
 
 def add_task():
