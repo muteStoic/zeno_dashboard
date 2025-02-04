05ca3978be4a3f180b831d47b8ebbd4a86480a66
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-05 00:23:22 +0800

    fsdf

--- Job_hunt.py
+++ Job_hunt.py
@@ -110,7 +110,7 @@ def chat_with_openai_text_and_image():
     model="gpt-4o-mini",
     messages=[
         {"role": "user","content": [{"type": "text","text": "What is in this image?",},{"type": "image_url","image_url": {"url": f"data:image/jpg;base64,{base64_image}"},},],}],)
-    print(response.choices[0])
+    st.write(response.choices[0])
 
     
    
