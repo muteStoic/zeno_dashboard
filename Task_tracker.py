1cf328d88187cdf08a545169d89e2a8343c8d733
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-04 16:45:52 +0800

    8

--- Task_tracker.py
+++ Task_tracker.py
@@ -9,7 +9,7 @@ st.cache_resource.clear()
 
 
 #//initialize the connection that is refered in the secrets toml
-conn = st.connection("google_service_account", type = GSheetsConnection)
+conn = st.connection("ghsheetsjob", type = GSheetsConnection)
 
 #//create variable that capture the information in the first sheet of the gsheetinto variable df
 df = conn.read(ttl="1m")
