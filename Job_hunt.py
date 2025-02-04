55ac9c324a51ab06650865f068503c663d6b9c41
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-04 17:48:16 +0800

    fdf

--- Job_hunt.py
+++ Job_hunt.py
@@ -64,9 +64,11 @@ with st.form("update data"):
     role2 = st.text_input('role' , key = "role2")
     st.form_submit_button("add", on_click = add_task)
 
+    #//using the streamlit data editor to displa the information from the sheet that is stored in "df" variable. dont know what is the key and num_rows is about.
+    data_edit = st.data_editor(df_job, use_container_width = True, key="my_key", num_rows = "dynamic" )
+
+
     
-#//using the streamlit data editor to displa the information from the sheet that is stored in "df" variable. dont know what is the key and num_rows is about.
-data_edit = st.data_editor(df_job, use_container_width = True, key="my_key", num_rows = "dynamic" )
 #*(used for testing)st.write(st.session_state["my_key"])
 
 #//streamlit button to run the function of updating the google sheet with the new data table
