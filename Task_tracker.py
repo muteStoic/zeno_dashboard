66613749250fd3d271eb69c5e08244913ffc121e
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-04 16:43:36 +0800

    9

--- Task_tracker.py
+++ Task_tracker.py
@@ -5,12 +5,7 @@ from streamlit_gsheets import GSheetsConnection
 #//remove all the browser cache
 st.cache_resource.clear()
 
-#homePage = st.Page("streamlit_app.py", title = "Homepage")
-job_hunt_page = st.Page("Job_hunt.py", title = "Job hunting")
-page_3 = st.Page("Page_3.py", title = "Blank empty page")
 
-pg = st.navigation([job_hunt_page,page_3])
-pg.run()
 
 
 #//initialize the connection that is refered in the secrets toml
