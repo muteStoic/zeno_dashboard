351906cfd35173c6dcac9abbc9f8636924c4ac7a
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-04 23:50:23 +0800

    dfdf

--- Job_hunt.py
+++ Job_hunt.py
@@ -100,7 +100,7 @@ def encode_image(image_path):
 
 def chat_with_openai_text_and_image():
     st.write(uploaded_file1)
-    image_path = uploaded_file1.type
+    image_path = uploaded_file1._file_urls
     st.write(image_path)
 
     # Getting the Base64 string
