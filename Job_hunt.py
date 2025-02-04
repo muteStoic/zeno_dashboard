da491a5874d1cf14cfb48014e4779e0bd22d6a6c
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-04 18:55:35 +0800

    er

--- Job_hunt.py
+++ Job_hunt.py
@@ -101,4 +101,4 @@ if uploaded_file is not None:
     st.write(dataframe)
 
     
-st.image("https://drive.google.com/file/d/18aK_yKOSJA4DuhI_v860_ymIGsSrNCg3/")
+st.image("https://enshrined.digigoat.co/wp-content/uploads/2024/12/border-test-e1733404981637.png")
