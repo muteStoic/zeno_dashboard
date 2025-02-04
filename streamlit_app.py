ee90cd6987370ea4b19b8e46331c6d3d6818656f
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-04 15:21:36 +0800

    put in the homepage app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,11 @@ from streamlit_gsheets import GSheetsConnection
 #//remove all the browser cache
 st.cache_resource.clear()
 
-
+homePage = st.Page("streamlit.app.py", title = "Homepage")
 job_hunt_page = st.Page("Job_hunt.py", title = "Job hunting")
 page_3 = st.Page("Page_3.py", title = "Blank empty page")
 
-pg = st.navigation([job_hunt_page,page_3])
+pg = st.navigation([homePage,job_hunt_page,page_3])
 pg.run()
 
 
