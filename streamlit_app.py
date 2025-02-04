53c7887625461ccc3ca9299ae2ebc5cc4fc04119
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-04 15:23:02 +0800

    edit the eror

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from streamlit_gsheets import GSheetsConnection
 #//remove all the browser cache
 st.cache_resource.clear()
 
-homePage = st.Page("streamlit.app.py", title = "Homepage")
+homePage = st.Page("streamlit_app.py", title = "Homepage")
 job_hunt_page = st.Page("Job_hunt.py", title = "Job hunting")
 page_3 = st.Page("Page_3.py", title = "Blank empty page")
 
