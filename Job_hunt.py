8040ea721929ff5484e48b46bcdad02d25cd0cc8
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-04 18:38:04 +0800

    43

--- Job_hunt.py
+++ Job_hunt.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
+from io import StringIO
 
 
 
@@ -84,5 +85,17 @@ st.button('update the sheet', on_click = update_sheet)
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
-    bytes_data = uploaded_file.geturl()
+    bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
