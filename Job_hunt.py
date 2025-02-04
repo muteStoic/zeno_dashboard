2dbe719929e94270ec706a225bb4dea0d0fdd982
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-04 16:41:23 +0800

    input ghseet for job

--- Job_hunt.py
+++ Job_hunt.py
@@ -7,7 +7,7 @@ from streamlit_gsheets import GSheetsConnection
 st.cache_resource.clear()
 
 #//initialize the connection that is refered in the secrets toml
-conn = st.connection("google_service_account", type = GSheetsConnection)
+conn = st.connection("gsheetsjob", type = GSheetsConnection)
 
 #//create variable that capture the information in the first sheet of the gsheetinto variable df
 df = conn.read(ttl="1m")
