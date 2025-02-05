2c2a751564ef64f041bca3a130b9ea98996a1d68
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-05 21:21:52 +0800

    123

--- Job_hunt.py
+++ Job_hunt.py
@@ -127,5 +127,5 @@ extracted_data = {
 #####
 ##
 company_full_information = extracted_data#change this data to response.choices[0].message.content for full running build
-st.write(company_full_information.value)
+st.write(company_full_information["Company Name"])
 
