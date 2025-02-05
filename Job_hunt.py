22ff5fce3d8009894b3f6f150452446394906529
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-05 17:20:46 +0800

    12

--- Job_hunt.py
+++ Job_hunt.py
@@ -119,7 +119,7 @@ def chat_with_openai_text_and_image():
     message = client.beta.threads.messages.create(
     thread_id=st.session_state.threadid,
     role="user",
-    content= base64_image
+    content = [{"type": "text","text": "what is 2 + 19",}]
     )
 
 
