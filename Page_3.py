f72d1df0aaf47afacd765f1e5b89dca9b802c81c
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-06 00:42:58 +0800

    12

--- Page_3.py
+++ Page_3.py
@@ -2,5 +2,12 @@ import streamlit as st
 import openai
 import base64
 from openai import Client
+from streamlit_gsheets import GSheetsConnection
 
-st.dataframe(st.session_state.fulljobdata)
+#//initialize the connection that is refered in the secrets toml
+conn = st.connection("google_service_account", type = GSheetsConnection)
+
+#//create variable that capture the information in the first sheet of the gsheetinto variable df
+df_job = conn.read(worksheet = "Sheet2")
+
+st.dataframe(df_job)
