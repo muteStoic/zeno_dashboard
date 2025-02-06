90aab5a6066bc61920191d9bbb659bb08d217450
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-06 18:47:48 +0800

    df

--- Page_3.py
+++ Page_3.py
@@ -12,15 +12,9 @@ df_job = conn.read(worksheet = "Sheet2")
 
 st.dataframe(df_job)
 
-testloc = df_job.at[1,"Job Title"]###
-st.write(df_job.shape[0])###
-st.write(testloc)###
-
 max_row = df_job.shape[0]
 for cont in range(max_row):
-    #def test():
-    #    df_job.at[cont,"Checkmark"] = True
-    #    st.dataframe(df_job)
+    
     checkfull = bool(df_job.at[cont, "Checkmark"])
     st.write(checkfull)
 
@@ -39,18 +33,9 @@ for cont in range(max_row):
             check = container_test.checkbox("Application submitted",value = bool(df_job.at[cont,"Checkmark"]), key = cont)
             if check:
                 df_job.at[cont,"Checkmark"] = True
-                st.dataframe(df_job)
+                conn.update(worksheet ="Sheet2", data = df_job)
+                
             expander_section = container_test.expander("Job Description")
             expander_section.write(df_job.at[cont, "Job Description"])
         
 
-    
-
-
-
-container1 = st.container(border = True)
-container1.write("testing")
-expander2 = container1.expander("test2")
-expander2.write("sdfwefwesd")
-expander1 = st.expander("click to open")
-expander1.write("testtesttesttesttesttesttest")
