3ab6a356f6bc7c0f230c3aac4db1b582e6439d44
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-06 18:15:16 +0800

    df

--- Page_3.py
+++ Page_3.py
@@ -19,9 +19,10 @@ st.write(testloc)###
 max_row = df_job.shape[0]
 for cont in range(max_row):
     def test():
-        st.write("complete")
+        df_job.at[cont,"URL link"] = True
 
     container_test = st.container(border = True)
+    container_test.write(df_job.at[cont,"URL link"])
     container_test.title(df_job.at[cont,"Job Title"])
     container_test.write("Company: " + df_job.at[cont,"Company Name"])
     container_test.write("Salary: " + df_job.at[cont, "Salary Range"])
