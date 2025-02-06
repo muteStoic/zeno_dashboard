300db74ce944f93f0735f76ba9c8cb3de5a6bdda
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-06 18:06:28 +0800

    d

--- Page_3.py
+++ Page_3.py
@@ -24,7 +24,7 @@ for cont in range(max_row):
     container_test.write("Company: " + df_job.at[cont,"Company Name"])
     container_test.write("Salary: " + df_job.at[cont, "Salary Range"])
     container_test.link_button("Go To Job", df_job.at[cont,"URL link"])
-    container_test.checkbox("Application submitted", key = cont)
+    container_test.checkbox("Application submitted",value = bool(df_job.at[cont,"Checkmark"]), key = cont)
     expander_section = container_test.expander("Job Description")
     expander_section.write(df_job.at[cont, "Job Description"])
 
