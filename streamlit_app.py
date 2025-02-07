98f5b61ecdfea6d0132a4216a2e0c3be8695fe09
muteStoic <50288669+muteStoic@users.noreply.github.com>
2025-02-07 16:58:40 +0800

    12

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,5 @@ restart_button = st.button("restart", on_click = restart)
 
 pg = st.navigation([task_tracker ,job_hunt_page,page_3, page_4, page_5])
 with st.sidebar:
-    restart_button
+    st.button("restart", on_click = restart)
 pg.run()
